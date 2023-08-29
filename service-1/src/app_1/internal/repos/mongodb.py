0fbfb81b087c6be20b10402996f3769974fc3f4a
Abhishek Rana <abhishekrana502@gmail.com>
2023-08-29 15:52:49 +0200

    Fix local deployment

--- service-1/src/app_1/internal/repos/mongodb.py
+++ service-1/src/app_1/internal/repos/mongodb.py
@@ -1,3 +1,5 @@
+import logging
+
 from beanie import init_beanie
 from motor.motor_asyncio import AsyncIOMotorClient  # type: ignore
 
@@ -7,6 +9,8 @@ from app_1.internal.models.products import ProductDB
 
 async def init_mongodb() -> None:
     config: MongoDBConfig = load_mongodb_config()
+    logging.info(f"Connecting to mongodb://{config.user}:*****@{config.host}:{config.port}/{config.database}")
+    print(f"Connecting to mongodb://{config.user}:*****@{config.host}:{config.port}/{config.database}")
 
     client = AsyncIOMotorClient(f"mongodb://{config.user}:{config.password}@{config.host}:{config.port}")
 
